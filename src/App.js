a25f895610f416db9c5f0face1a3a9a4049dedb6
CezarDev <bacanieskidev@gmail.com>
2024-07-12 20:08:36 -0300

    subindo codigo e api json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [racas, setRacas] = useState([]);
+  const [busca, setBusca] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/doguinhos')
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setRacas(dados);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (busca && busca.length > 3) {
+      fetch('http://localhost:8080/doguinhos?nome=' + busca)
+        .then((resposta) => resposta.json())
+        .then((dados) => {
+          setRacas(dados);
+        });
+    }
+  }, [busca]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Vou Conseguir ser um Software Engineer Fullstack!</h1>
+      <h4>Confira abaixo uma lista de raça de cachorros</h4>
+      <input
+        on
+        placeholder='Buscar por raça'
+        onChange={(event) => setBusca(event.target.value)}
+      ></input>
+      <ul>
+        {racas.map((raca) => (
+          <li key={raca.nome}>{raca.nome}</li>
+        ))}
+      </ul>
     </div>
   );
 }
